45e010300b5c73d15574eea0cabd5a7e71ce5a0a
Carolyne Golias <goliascarolyne@gmail.com>
2025-04-29 11:33:14 -0700

    working on nav bar

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,6 +1,9 @@
+import { NavBar } from "./NavBar";
+
 export function Main(){
     return (<>
     <div className="@container w-full h-screen bg-olive/90">
+    <NavBar/>
     </div>
     </>)
 }
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+
+export function NavBar(){
+    return <>
+    <nav className="h-20 sm:h-24 md:h-24 w-full bg-white flex shadow-md">
+        <div className="items-end">
+        </div>
+    </nav>
+    </>
+}
