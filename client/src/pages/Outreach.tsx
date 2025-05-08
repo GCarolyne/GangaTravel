eb5be4734acca380b78b6c6ad0dcd96f270c62a3
Carolyne Golias <goliascarolyne@gmail.com>
2025-05-08 13:26:31 -0700

    working on form to support customer outreach

--- client/src/pages/Outreach.tsx
+++ client/src/pages/Outreach.tsx
@@ -2,9 +2,21 @@ export function Outreach() {
   return (
     <>
       <div className="@container w-full h-screen bg-olive/70">
-      <form>
-        
-      </form>
+        <form action="" className="w-full">
+          <div className="w-full">
+            <label htmlFor="namedInput">Meno:</label>
+            <input id="namedInput" type="text" name="name" required />
+          </div>
+          <div className="w-full">
+            <label htmlFor="email">Zadajte svoj email:</label>
+            <input type="email" name="email" id="email" required />
+          </div>
+          <div className="w-full">
+            <label htmlFor="phoneNumber">
+              <input type="number" name="phoneNumber" id="phoneNumber"></input>
+            </label>
+          </div>
+        </form>
       </div>
     </>
   );
